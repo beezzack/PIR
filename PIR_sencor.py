5093e8957dda9c3a1669f57c408733ad0c43d8bc
beezzack <cliff860429@gmail.com>
2018-03-22 21:28:41 +0900

    origin

---
+++ PIR_sencor.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+def setup(GPIOnum):
+  GPIO.setmode(GPIO.board)
+  GPIO.setup(GPIOnum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+def motion(GPIOnum):
+    global counter
+
+    if GPIO.input)=(GPIOnum):
+        counter += 1
+        print("Motion detected {0}".format(counter))
+
+    else:
+        print("Motion not detected")
+
+try:
+    setup(14)
+    GPIO.add_event_detect(14, GPIO.BOTH, callback=motion, bouncetime=200)
+    while True:
+        time.sleep(1)
+except:
+    GPIO.cleaup()
